d002a0df373f9fd189cd07654a141efe5b2b67d5
bkisly <b.kisly@gmail.com>
2023-10-03 08:46:11 +0200

    Configured database

---
+++ BlazorExercise/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorExercise.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>  options) : base(options) 
+        { }
+    }
+}
